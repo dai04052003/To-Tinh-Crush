64743e259c641fc4365b4028884384c8940431e3
dai04052003 <86762299+dai04052003@users.noreply.github.com>
2021-06-30 23:18:47 -0700

    Update config.js

--- config.js
+++ config.js
@@ -15,5 +15,5 @@ const CONFIG = {
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
     messDesc: 'Tối nay 21h anh qua đón em đi hóng gió nhé!.',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com'https://www.facebook.com/messages/t/100025613041225 //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'http://fb.com' https://www.facebook.com/nguyendinhdai.okela
 }
